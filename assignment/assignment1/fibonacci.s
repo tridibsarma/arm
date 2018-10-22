ec051cbe926067c26f59e0fcd9eda1f4e4d758f0
TRIDIB SARMA <44354950+tridibsarma@users.noreply.github.com>
2018-10-22 22:59:42 +0530

    Update and rename fibonacci to fibonacci.s

--- assignment/assignment1/fibonacci.s
+++ assignment/assignment1/fibonacci.s
@@ -5,12 +5,12 @@ __main  function
 	          MOV r0 , #0  ;f(0) = 0
 	          MOV r1 , #1    ; f(1) = 1
               MOV r7 , #1	  ; number to calculate fibonacci series 
-              MOV r2 , r7	 ; R2 will store output value 		  
+              MOV r2 , r7	 ; R2 for storing output value 		  
               CMP r2 , #1
               IT LS 
               BLS STOP				  
 			  SUB r3 , r2 ,#1      ; LOOP COUNT R3
-LOOP              ADD r4 , r1 , r0  ;R4 to hold  value of f(n-1)+ f(n-2) 
+LOOP              ADD r4 , r1 , r0  ; R4 to hold  value of f(n-1)+ f(n-2) 
                   MOV r0 ,r1
                   MOV r1 ,r4
                   MOV r2 , r4

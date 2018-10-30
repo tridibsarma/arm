2078158c08c5439e552e797ff614fdc688ed385a
TRIDIB SARMA <44354950+tridibsarma@users.noreply.github.com>
2018-10-30 22:39:22 +0530

    Update exponential.s

--- assignment/assignment4/exponential.s
+++ assignment/assignment4/exponential.s
@@ -5,20 +5,20 @@ PRESERVE8
 	 ENTRY 
 __main  FUNCTION
 
-	VMOV.F32 S1, #4;x-Number to find e^x
-	VMOV.F32 S2, #15;Number of terms been considered in e^x expansion
-	VMOV.F32 S3, #1;count
-	VMOV.F32 S4, #1; temp
-	VMOV.F32 S5, #1; result
-	VMOV.F32 S7, #1;register to hold one
+	VMOV.F32 S1, #5;	x-Number to find e^x
+	VMOV.F32 S2, #10;	Number of terms been considered in e^x expansion
+	VMOV.F32 S3, #1;	count
+	VMOV.F32 S4, #1; 	temp
+	VMOV.F32 S5, #1; 	result
+	VMOV.F32 S7, #1;	register to hold one
 Loop 
-	 VCMP.F32 S2, S3;Comparison done for excuting taylor series expansion of e^x for s2 number of terms
-	 VMRS.F32 APSR_nzcv,FPSCR;Used to copy fpscr to apsr
+	 VCMP.F32 S2, S3;	Comparison done for excuting taylor series expansion of e^x for s2 number of terms
+	 VMRS.F32 APSR_nzcv,FPSCR;	Used to copy fpscr to apsr
 	 BLT stop;
-	 VDIV.F32 S6, S1, S3; temp1=x/count
-	 VMUL.F32 S4, S4, S6; temp=temp*temp1;
-	 VADD.F32 S5, S5, S4; result=result+temp;
-	 VADD.F32 S3, S3, S7; incrementing count
+	 VDIV.F32 S6, S1, S3; 	temp1=x/count
+	 VMUL.F32 S4, S4, S6; 	temp=temp*temp1;
+	 VADD.F32 S5, S5, S4; 	result=result+temp;
+	 VADD.F32 S3, S3, S7; 	incrementing count
 	 B Loop; 
 	 
 stop B stop ; stop program
